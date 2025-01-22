01b66e0ae0bddb3b79ad43f2bb4e5201672ee7f3
PearlGrell <aryantrivedi.lko@outlook.com>
2025-01-23 00:07:38 +0530

    commit :: 2

--- build/middleware/error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = error;
-function error(err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send({
-        error: err.message,
-    });
-}
